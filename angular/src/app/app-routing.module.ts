a38d53419a2d2a406acacf21c83cf7637653761b
Oskar Hansson <oskis101@gmail.com>
2018-04-18 13:28:59 +0200

    Added auth.guard

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,13 +1,16 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth.guard'
 
 import { FrontpageComponent }   from './frontpage/frontpage.component';
 import { LoginComponent }       from './login/login.component';
 
+
 const routes: Routes = [
   { path: '', redirectTo: '/frontpage', pathMatch: 'full'},
   { path: 'frontpage', component: FrontpageComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent },
+  // { path: 'test', component: LoginComponent, canActivate: [AuthGuard]} // AuthGuard example
 ]
 
 @NgModule({
