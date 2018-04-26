d29b23ed2c95fa864f09de66ab459c1a3331a71a
rekermann <38425112+rekermann@users.noreply.github.com>
2018-04-26 23:35:19 +0200

    Add files via upload

--- angular/src/app/addproduct/addproduct.component.ts
+++ angular/src/app/addproduct/addproduct.component.ts
@@ -4,6 +4,8 @@ import { Category } from '../services/category';
 import { Observable } from 'rxjs/observable';
 import { CategoryService } from '../services/category.service';
 import { ItemService } from '../services/item.service';
+import { CookieService } from 'ngx-cookie-service';
+import { AngularFireDatabase } from 'angularfire2/database';
 
 @Component({
   selector: 'app-addproduct',
@@ -12,9 +14,12 @@ import { ItemService } from '../services/item.service';
 })
 export class AddproductComponent implements OnInit {
 	isVisible: boolean = false;
+	isAdmin: boolean = false;
 	constructor(
 		private categoryService : CategoryService,
 		private itemService : ItemService,
+		private cookieService: CookieService, 
+		private db: AngularFireDatabase,
 	) { }
 
 	model = new Item();
@@ -31,6 +36,17 @@ export class AddproductComponent implements OnInit {
 
 	ngOnInit() {
 		this.categories = this.categoryService.getCategories();
+		
+		const UID: string = this.cookieService.get('UID');
+	    this.db.object(`users/` + UID + `/admin`).valueChanges().subscribe((value) => {
+	    if(value === 'true'){
+		this.adminTrue(); 
+	 }});	
+	}
+	
+	
+	adminTrue() {
+		this.isAdmin = true;
 	}
 
 	toggleVisible() {
