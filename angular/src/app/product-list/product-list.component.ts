ca297b1e2163383acb8381c4f1c5d4435d2bb0ca
palm <tauman.exe@gmail.com>
2018-05-04 10:42:01 +0200

    tiny fix for presentation. a bit hax. Easy to revert

--- angular/src/app/product-list/product-list.component.ts
+++ angular/src/app/product-list/product-list.component.ts
@@ -202,6 +202,6 @@ export class ProductListComponent implements OnInit {
    * @returns {number}
    */
   private computeBreakPoints(width: number) {
-    return width < 768 ? 1 : 1;
+    return width < 768 ? 1 : Math.floor((width - 320) / 220);
   }
 }
