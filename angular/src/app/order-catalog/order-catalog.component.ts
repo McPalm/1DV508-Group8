1808fba9e5c3d569184f47dabd431c19f5e06167
palm <tauman.exe@gmail.com>
2018-05-15 18:55:00 +0200

    Added a component for listing all orders

---
+++ angular/src/app/order-catalog/order-catalog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-catalog',
+  templateUrl: './order-catalog.component.html',
+  styleUrls: ['./order-catalog.component.css']
+})
+export class OrderCatalogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
