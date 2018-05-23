f487e436bac38c4df9e9c7b218d17f72c1845be5
Voldie <voldiekami@gmail.com>
2018-05-23 20:41:10 +0200

    Started adding child route instead of branches of if statements.

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -4,19 +4,33 @@ import { AuthGuard } from './core/auth.guard'
 
 import { FrontpageComponent }   from './frontpage/frontpage.component';
 import { LoginComponent }       from './login/login.component';
+import {NavComponent} from "./nav/nav.component";
+import {Profile} from "selenium-webdriver/firefox";
+import {UserprofileComponent} from "./userprofile/userprofile.component";
+import {AdminComponent} from "./admin/admin.component";
+import {OrderCatalogComponent} from "./order-catalog/order-catalog.component";
+import {ProductListComponent} from "./product-list/product-list.component";
+import {ItemdetailsComponent} from "./itemdetails/itemdetails.component";
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/frontpage', pathMatch: 'full'},
   { path: 'frontpage', component: FrontpageComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'main', component: NavComponent, children: [
+    {path: 'profile', component: UserprofileComponent},
+    {path: 'admin', component: AdminComponent},
+    {path: 'order', component: OrderCatalogComponent},
+    {path: 'product', component: ProductListComponent},
+    {path: 'product/:id', component: ItemdetailsComponent},
+    ]},
   // { path: 'test', component: LoginComponent, canActivate: [AuthGuard]} // AuthGuard example
 ]
 
 @NgModule({
   imports: [
     RouterModule.forRoot(routes)
-  ], 
+  ],
   exports: [
     RouterModule
   ]
