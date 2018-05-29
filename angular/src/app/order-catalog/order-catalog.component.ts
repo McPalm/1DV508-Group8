5ad64f8a312a9719eebf4c5105c32a7102b85ee1
rekermann <38425112+rekermann@users.noreply.github.com>
2018-05-30 00:12:25 +0200

    Add files via upload

--- angular/src/app/order-catalog/order-catalog.component.ts
+++ angular/src/app/order-catalog/order-catalog.component.ts
@@ -4,6 +4,9 @@ import { Item } from '../services/item';
 import { Order } from '../services/order';
 import { OrderService } from '../services/order.service';
 import { Observable } from 'rxjs/Observable';
+import { EmailService } from '../services/email.service';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-order-catalog',
@@ -18,10 +21,11 @@ export class OrderCatalogComponent implements OnInit {
   order : Order;
   totalPrice : number;
   userName : string;
+  reason : string;
 
   // orderss : Observable<Order[]>;
 
-  constructor(private orderService : OrderService) { }
+  constructor(private orderService : OrderService, private es: EmailService, private db: AngularFireDatabase, private CookieService: CookieService) { }
 
   ngOnInit() {
     this.orders = this.orderService.getOrders();
@@ -39,11 +43,81 @@ export class OrderCatalogComponent implements OnInit {
   acceptOrder() {
     console.log("Yay, we accept the order!");
     // TODO, basically everything related to actually confirming the order
+	
+	let userID = this.order.userid
+	let UID = this.order.uid;
+	
+	let dbRef = this.db.object(`users/${userID}/email`).valueChanges().subscribe((value) => {
+	
+	
+	
+	const data = {
+		toReason : this.reason,
+		toOrder : this.order.ordernumber,
+		toAdress : this.order.address1,
+		toName : this.order.name,
+		toEmail : value,
+		
+	}
+	
+	
+	this.es.accEmail(data);
+	
+	let opt = 'accepted';
+	this.adminMail(opt, this.order.ordernumber);
+	
+	this.db.object(`orders/${UID}`).remove();
+	
+	this.changeActive(this.order[0])
+	
+	dbRef.unsubscribe();
+	
+	});
+	
+	
+	
+	
+	
   }
 
   rejectOrder() {
     console.log("We rejected the order! :<")
     // TODO, you know
+	
+	let userID = this.order.userid;
+	let UID = this.order.uid;
+	
+	let dbRef = this.db.object(`users/${userID}/email`).valueChanges().subscribe((value) => {
+	
+	
+	
+	const data = {
+		toReason : this.reason,
+		toOrder : this.order.ordernumber,
+		toName : this.order.name,
+		toEmail : value,
+		
+	}
+	
+	
+	this.es.rejectEmail(data);
+	
+	let opt = 'rejected';
+	
+	this.adminMail(opt, this.order.ordernumber);
+	
+	
+	this.db.object(`orders/${UID}`).remove();
+	
+	this.changeActive(this.order[0]);
+	
+	
+	
+	 dbRef.unsubscribe();
+	});
+	
+	
+
   }
 
   totalPriceOf(order: Order) : number {
@@ -53,5 +127,36 @@ export class OrderCatalogComponent implements OnInit {
   
     return sum;
   }
+  
+  
+  adminMail(opt , orderid) {
+	  
+	  
+	  
+	  
+	  let userID = this.CookieService.get('UID');
+	  
+	  
+	  
+	  let dbRef = this.db.object(`users/${userID}/email`).valueChanges().subscribe((value) => {
+		  
+		  console.log(userID);
+		  
+		let data = {
+			toEmail : value,
+			toSubject : 'Order ID: ' + orderid,
+			toText : 'Order with Order ID ' + orderid + 'has been ' + opt + ' please proceed accordingly',
+			
+		}
+	  
+		console.log('tt');
+		this.es.sendCustomEmail(data);
+		
+		dbRef.unsubscribe();
+	  });
+	  
+  }
+  
+  
   
 }
